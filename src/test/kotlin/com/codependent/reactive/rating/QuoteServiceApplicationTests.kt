adf6f6f284ef605ad209c43fe3c2ebdcafaa985b
Víctor Martín Molina <vmartin@profile.es>
2020-06-15 17:06:11 +0200

    e2e test

--- src/test/kotlin/com/codependent/reactive/rating/QuoteServiceApplicationTests.kt
+++ src/test/kotlin/com/codependent/reactive/rating/QuoteServiceApplicationTests.kt
@@ -1,13 +1,45 @@
-package com.codependent.reactive.ratingservice
+package com.codependent.reactive.rating
 
+import com.codependent.reactive.rating.controller.Rating
+import com.codependent.reactive.rating.controller.RatingRequest
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.messaging.rsocket.RSocketRequester
+import reactor.core.publisher.Mono
+import reactor.kotlin.test.test
+import java.net.URI
 
 @SpringBootTest
+@TestInstance(PER_CLASS)
 class RatingServiceApplicationTests {
+    var requester: RSocketRequester? = null
+
+    @BeforeAll
+    fun setupOnce(@Autowired builder: RSocketRequester.Builder) {
+        requester = builder
+                .connectWebSocket(URI.create("http://localhost:8080/rating-ws"))
+                .block()
+    }
 
     @Test
     fun contextLoads() {
+    }
+
+    @Test
+    fun testRequestGetsResponse() {
+        val ratingRequest = RatingRequest("songId")
+        val result: Mono<Rating> = requester!!
+                .route("request-rating")
+                .data(ratingRequest)
+                .retrieveMono(Rating::class.java)
+        result.test()
+                .expectNextMatches {
+                    it.songId == "songId"
+                }.verifyComplete()
     }
 
 }
