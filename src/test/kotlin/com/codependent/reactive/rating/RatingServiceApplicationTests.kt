d4ec23b0e629d9addfdcd6e20d84c094bff9adba
jainigo <jainigo@profile.es>
2020-06-15 17:21:12 +0200

    updated test

--- src/test/kotlin/com/codependent/reactive/rating/RatingServiceApplicationTests.kt
+++ src/test/kotlin/com/codependent/reactive/rating/RatingServiceApplicationTests.kt
@@ -7,17 +7,21 @@ import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
+import org.springframework.boot.web.server.LocalServerPort
 import org.springframework.messaging.rsocket.RSocketRequester
 import reactor.core.publisher.Mono
 import reactor.kotlin.test.test
 import java.net.URI
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 @TestInstance(PER_CLASS)
-class RatingServiceApplicationTests(@Autowired private val builder: RSocketRequester.Builder) {
+class RatingServiceApplicationTests(
+        @Autowired private val builder: RSocketRequester.Builder,
+        @LocalServerPort private val serverPort: Int) {
 
     private val requester: RSocketRequester = builder
-            .connectWebSocket(URI.create("http://localhost:8080/rating-ws"))
+            .connectWebSocket(URI.create("http://localhost:$serverPort/rating-ws"))
             .block()!!
 
     @Test
